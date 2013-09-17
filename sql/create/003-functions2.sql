fd1b4f2f7b8e1b53b00854a6ad8c4a90372f4297
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-09-17 08:21:03 -0300

    Fix parameters of function has_permission

--- sql/create/003-functions2.sql
+++ sql/create/003-functions2.sql
@@ -5,6 +5,6 @@ CREATE OR REPLACE FUNCTION has_permission(_user_id integer,
                                           _exp_name text,
                                           _type text default 'read') RETURNS boolean AS
 $$
-    SELECT has_permission(_user_id, (SELECT id FROM experiments WHERE name = _exp_name), _type);
+    SELECT has_permission($1, (SELECT id FROM experiments WHERE name = $2), $3);
 $$
 LANGUAGE SQL;
