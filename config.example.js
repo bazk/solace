747fe68c270d6875a3a9613527d5caebdd132205
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-09-17 08:18:31 -0300

    Move config.js to config.example.js

---
+++ config.example.js
@@ -0,0 +1,7 @@
+exports.db_config = {
+    user: 'user',
+    password: 'password',
+    database: 'dbname',
+    host: 'localhost',
+    port: 5432
+};
--- config.js
+++
@@ -1,7 +0,0 @@
-exports.db_config = {
-    user: 'solace',
-    password: 'zxASd0&.AS)D(-=Asd098C',
-    database: 'solace',
-    host: 'localhost',
-    port: 5432
-};
